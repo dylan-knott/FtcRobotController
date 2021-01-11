3f9d8045cb98aad38c8263e230f3b894114b78a8
Dylan Knott <dgknott33@gmail.com>
2021-01-11 08:33:17 -0600

    Tuning Roadrunner for accuracy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,8 +31,12 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 14.5738; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 14.5253; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 7.5; // in; offset of the lateral wheel
+
+    public static double X_MULTIPLIER = 1; //Multiplier in the X direction
+    public static double Y_MULTIPLIER = 1; //Multiplier in the Y direction
+
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
@@ -59,9 +63,9 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     @Override
     public List<Double> getWheelPositions() {
         return Arrays.asList(
-                encoderTicksToInches(leftEncoder.getCurrentPosition()),
-                encoderTicksToInches(rightEncoder.getCurrentPosition()),
-                encoderTicksToInches(frontEncoder.getCurrentPosition())
+                encoderTicksToInches(leftEncoder.getCurrentPosition()) * X_MULTIPLIER,
+                encoderTicksToInches(rightEncoder.getCurrentPosition()) * X_MULTIPLIER,
+                encoderTicksToInches(frontEncoder.getCurrentPosition()) * Y_MULTIPLIER
         );
     }
 
